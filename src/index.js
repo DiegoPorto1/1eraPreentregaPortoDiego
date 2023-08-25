a1c1938ab7e1a655da1c8ec7ac429a79c6d3ce82
DiegoPorto1 <Diego566_@hotmail.com>
2023-08-24 21:27:35 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import express from "express"
 import prodsRouter from "./routes/products.routes.js";
 import { __dirname } from "./path.js";
 import path from 'path';
-
+import ProductManager from "./services/ProductoManager.js";
+import cartRouter from "./routes/carts.routes.js";
 const app = express()
 const PORT =8080
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({extended:true}))
 
 //Routes
 app.use('/api/products', prodsRouter)
+app.use('/api/carts', cartRouter)
 app.use('/static',express.static(path.join(__dirname, '/public')))
 
 app.listen(PORT, ()=> {
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import ProductManager from "../services/ProductoManager.js";
+ 
 
+const productManager = new ProductManager
 const prodsRouter = Router()
 
  prodsRouter.get('/', async (req, res)=> {
@@ -19,28 +22,27 @@ res.status (200).send (products)
     const prod = await productManager.getProductsById (parseInt(id))
 
      if(prod) 
-   res.status (200),send(prod)
+        res.status (200).send(prod)
     else 
-res.status(404).send("Producto no encontrado")
+       res.status(404).send("Producto no encontrado")
 
  })
 
  prodsRouter.post('/', async(req,res) =>{
-    const {code} = req.body
-    const confirmacion = await  productManager.getProductsBycode(code)
+    
+    const confirmacion = await  productManager.addProduct(req.body)
     if (confirmacion) {
-        req.status(404),send("Producto ya creado")
+        req.status(200).send("Product creado")
     
     } else  {
-        const conf = await productManager.addProduct(req.body)
-        if (conf)
-        res.status(200).send("Producto creado")
+      
+        res.status(400).send("Producto no creado")
     }
 
  })
  prodsRouter.put('/:id', async(req,res) =>{
     const {id} = req.params;
-    const confirmacion = await  productManager.getProductsByID (parseInt(id))
+    const confirmacion = await  productManager.updateProduct (parseInt(req.params.id), req.body)
     if (confirmacion) {
        await productManager.updateProduct(parseInt(id), req.body)
        res.status(200).send("Producto Actualizado")
@@ -52,8 +54,8 @@ res.status(404).send("Producto no encontrado")
  })
 
  prodsRouter.delete('/:id', async(req,res) =>{
-    const {id} = req.params;
-    const confirmacion = await  productManager.getProductsByID (parseInt(id))
+    
+    const confirmacion = await  productManager.deleteProduct (parseInt(req.params.id))
     if (confirmacion) {
        await productManager.deleteProduct(parseInt(id), req.body)
        res.status(200).send("Producto eliminado")
